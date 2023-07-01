a2d229b22fd992083d8e2b55d9088bac85ebf199
Yekteryna <128062849+Yekteryna@users.noreply.github.com>
2023-07-01 17:42:32 +0200

    translations

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -10,6 +10,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 /**
  * CategoryController.
@@ -18,6 +19,7 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/category')]
 class CategoryController extends AbstractController
 {
+    private TranslatorInterface $translator;
     /**
      * Index action.
      *
